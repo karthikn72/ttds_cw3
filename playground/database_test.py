a10d7fb6f337acf3ead81450e365163906236e00
Karthik <karthik-narayan@hotmail.com>
2024-02-02 20:41:19 +0000

    Add a test table to the database

--- playground/database_test.py
+++ playground/database_test.py
@@ -3,24 +3,20 @@ import os
 from google.cloud.sql.connector import Connector, IPTypes
 import pg8000
 
-import sqlalchemy
+import sqlalchemy as db
 
 
-def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+def connect_with_connector() -> db.engine.base.Engine:
     """
     Initializes a connection pool for a Cloud SQL instance of Postgres.
 
     Uses the Cloud SQL Python Connector package.
     """
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
 
-    instance_connection_name = "sentinews-413116:europe-west2:sentinews-db" # e.g. 'project:region:instance'
-    db_user = 'postgres'  # e.g. 'my-db-user'
-    db_pass = 'senti2024'  # e.g. 'my-db-password'
-    db_name = 'news_db'  # e.g. 'my-database'
+    instance_connection_name = "sentinews-413116:europe-west2:sentinews-db"
+    db_user = 'postgres'
+    db_pass = 'senti2024'
+    db_name = 'news_db'
 
     ip_type = IPTypes.PUBLIC
 
@@ -40,13 +36,29 @@ def connect_with_connector() -> sqlalchemy.engine.base.Engine:
 
     # The Cloud SQL Python Connector can be used with SQLAlchemy
     # using the 'creator' argument to 'create_engine'
-    pool = sqlalchemy.create_engine(
+    pool = db.create_engine(
         "postgresql+pg8000://",
         creator=getconn,
     )
     print("Success")
-    return pool
+    return pool 
 
-with connect_with_connector().connect() as conn:
-    result = conn.execute("SELECT 1")
-    print(result.fetchone()[0])
+with connect_with_connector().connect() as db_conn:
+    metadata = db.MetaData()
+    print(metadata)
+    Articles = db.Table('Articles', metadata,
+                db.Column('Id', db.Integer(),primary_key=True),
+                db.Column('Author', db.String(255), nullable=True),
+                db.Column('Title', db.String(500), nullable=False),
+                db.Column('Article', db.String(), nullable=False),
+                db.Column('URL', db.String(), nullable=True),
+                db.Column('Section', db.String(), nullable=False),
+                db.Column('Publication', db.String(), nullable=False)
+                )
+
+    metadata.create_all(db_conn)
+    query = db.insert(Articles).values(Id=1, Author='John', Title='The Great Escape', Article='Yeetus feetus lorem ipsum', URL='yeet', Section='Sports', Publication='Guardian')
+    result = db_conn.execute(query)
+    output = db_conn.execute(Articles.select()).fetchall()
+    print(output)
+    print(metadata.tables.keys())
