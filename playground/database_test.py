fe9a53fe0388b8ce82da8252469e5afa7191442e
karthikn72 <karthik-narayan@hotmail.com>
2024-02-06 00:54:51 +0000

    Try creating articles tables and add article

--- playground/database_test.py
+++ playground/database_test.py
@@ -1,59 +1,10 @@
-import os
 
-from google.cloud.sql.connector import Connector, IPTypes
-import pg8000
+from tools import database
 
-import sqlalchemy as db
-
-def connect_with_connector() -> db.engine.base.Engine:
-    """
-    Initializes a connection pool for a Cloud SQL instance of Postgres.
-
-    Uses the Cloud SQL Python Connector package.
-    """
-
-    instance_connection_name = "sentinews-413116:europe-west2:sentinews-db"
-    db_user = 'postgres'
-    db_pass = 'senti2024'
-    db_name = 'news_db'
-
-    ip_type = IPTypes.PUBLIC
-    connector = Connector()
-
-    def getconn() -> pg8000.dbapi.Connection:
-        conn: pg8000.dbapi.Connection = connector.connect(
-            instance_connection_name,
-            "pg8000",
-            user=db_user,
-            password=db_pass,
-            db=db_name,
-            ip_type=ip_type,
-        )
-        return conn
-
-    pool = db.create_engine(
-        "postgresql+pg8000://",
-        creator=getconn,
-    )
-    print("Connected to database successfully")
-    return pool 
-
-with connect_with_connector().connect() as db_conn:
-    metadata = db.MetaData()
-    print(metadata)
-    Articles = db.Table('Articles', metadata,
-                db.Column('Id', db.Integer(),primary_key=True),
-                db.Column('Author', db.String(255), nullable=True),
-                db.Column('Title', db.String(500), nullable=False),
-                db.Column('Article', db.String(), nullable=False),
-                db.Column('URL', db.String(), nullable=True),
-                db.Column('Section', db.String(), nullable=False),
-                db.Column('Publication', db.String(), nullable=False)
-                )
-
-    metadata.create_all(db_conn)
-    query = db.insert(Articles).values(Id=1, Author='John', Title='The Great Escape', Article='Yeetus feetus lorem ipsum', URL='yeet', Section='Sports', Publication='Guardian')
-    result = db_conn.execute(query)
-    output = db_conn.execute(Articles.select()).fetchall()
-    print(output)
-    print(metadata.tables.keys())
+if __name__ == "__main__":
+    db = database.Database()
+    db.create_article_table()
+    print(db.get_tables())
+    db.insert_article(1, 5, 'The Fox', 'Once upon on a time, far far away there lived a sly fox.', 'www.google.com', 6)
+    print(db.get_tables())
+    print("Database test completed successfully")
--- tools/database.py
+++ tools/database.py
@@ -48,26 +48,36 @@ class Database:
             metadata = db.MetaData()
             articles = db.Table('articles', metadata,
                         db.Column('article_id', db.Integer(),primary_key=True),
-                        db.Column('author_id', db.String(255), nullable=True),
+                        db.Column('author_id', db.Integer(), nullable=True),
                         db.Column('title', db.String(500), nullable=False),
                         db.Column('article', db.String(), nullable=False),
                         db.Column('url', db.String(), nullable=True),
-                        db.Column('publication_id', db.String(), nullable=False)
+                        db.Column('publication_id', db.Integer(), nullable=False)
                         )
 
             metadata.create_all(db_conn)
             db_conn.commit()
 
-            query = db.insert(Articles).values(Id=1, Author='John', Title='The Great Escape', Article='Yeetus feetus lorem ipsum', URL='yeet', Section='Sports', Publication='Guardian')
-            result = db_conn.execute(query)
-            output = db_conn.execute(Articles.select()).fetchall()
-            print(output)
-            print(metadata.tables.keys())
-
     def get_tables(self):
         return self.engine.table_names()
     
-    def insert_article(self, article_id, author_id, title, article, url, publication_id):
+    def insert_article(self, 
+                       article_id, 
+                       author_id, 
+                       title, 
+                       article, 
+                       url, 
+                       publication_id):
+        
         with self.engine.connect() as db_conn:
-            metadata = db.MetaData(bind=db_conn)
+            metadata = db.MetaData()
+            articles = db.Table('articles', metadata, autoload_with=db_conn)
+            query = db.insert(articles).values(article_id=article_id,
+                                               author_id=author_id, 
+                                               title=title, 
+                                               article=article, 
+                                               url=url, 
+                                               publication_id=publication_id)
+            db_conn.execute(query)
+            print(db_conn.execute(articles.select()).fetchall())
 
