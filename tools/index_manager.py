4643c765c646652c7ce21c8c87a4994905026522
SIMONDSKNIGHT <knigsd@icloud.com>
2024-02-10 01:06:00 +0900

    Update index_manager.py
    man I really goobed this one

--- tools/index_manager.py
+++ tools/index_manager.py
@@ -13,186 +13,16 @@ def firstThousand(dataset_file):
 
 # additionally, the function should put the new posting in the right place alphabetically within the index
 # and merge the postings if the word is already in the index
-def add_to_index(filepath, sorted_postings_file):
-    with open(filepath, 'r') as file1:
-        data1 = file1.read().splitlines()
-    with open(sorted_postings_file, 'r') as file2:
-        data2 = file2.read().splitlines()
-    i,j = 0,0
-    merged = []
-    while i < len(data1) and j < len(data2):
-        if data1[i].split(':')[0] < data2[j].split(':')[0]:
-            merged.append(data1[i])
-            i += 1
-            while i < len(data1):
-                if data1[i].startswith('   '):
-                    merged.append(data1[i])
-                    i += 1
-                else:
-                    break
-        elif data1[i].split(':')[0] == data2[j].split(':')[0]:
-            word = data1[i].split(':')[0]
-            i += 1
-            j += 1
-            """# if the docs are always different
-            count = int(data1[i].split(':')[1]) + int(data2[j].split(':')[1])
-            merged.append(f'{word}:{count}')
-            while not data1[i].startswith('   ') and not data2[j].startswith('   '):
-                if data1[i]< data2[j]:
-                    merged.append(data1[i])
-                    i += 1
-                else:
-                    merged.append(data2[j])
-                    j += 1
-            while true:
-                if data1[i].startswith('   '):
-                    merged.append(data1[i])
-                    i += 1
-                else:
-                    break
-            while true:
-                if data2[j].startswith('   '):
-                    merged.append(data2[j])
-                    j += 1
-                else:
-                    break
-            """
-            #if the docs can be the same
-            dict1 = {}
-            while i < len(data1) and j < len(data2):
-                if data1[i].startswith('   ') and data2[j].startswith('   '):
-                    if data1[i].split(':')[0] < data2[j].split(':')[0]:
-                        doc = data1[i][3:].split(':')[0]
-                        positions = [int(pos) for pos in data1[i][3:].split(':')[1].split(',')]
-                        dict1[doc] = positions
-                        i += 1
-                    elif data1[i].split(':')[0] == data2[j].split(':')[0]:
-                        doc = data1[i][3:].split(':')[0]
-                        positions1 = [int(pos) for pos in data1[i][3:].split(':')[1].split(',')]
-                        positions2 = [int(pos) for pos in data2[j][3:].split(':')[1].split(',')]
-                        pos1=0
-                        pos2=0
-                        dict1[doc] = []
-                        while pos1 < len(positions1) and pos2 < len(positions2):
-                            if positions1[pos1] < positions2[pos2]:
-                                dict1[doc].append(positions1[pos1])
-                                pos1 += 1
-                            elif positions1[pos1] == positions2[pos2]:
-                                dict1[doc].append(positions1[pos1])
-                                pos1 += 1
-                                pos2 += 1
-                            else:
-                                dict1[doc].append(positions2[pos2])
-                                pos2 += 1
-                        while pos1 < len(positions1):
-                            dict1[doc].append(positions1[pos1])
-                            pos1 += 1
-                        while pos2 < len(positions2):
-                            dict1[doc].append(positions2[pos2])
-                            pos2 += 1
-                        i += 1
-                        j += 1
-                    else:
-                        doc = data2[j][3:].split(':')[0]
-                        positions = [int(pos) for pos in data2[j][3:].split(':')[1].split(',')]
-                        dict1[doc] = positions
-                        j += 1
-                else:
-                    break
-            while i < len(data1):
-                if data1[i].startswith('   '):
-                    doc = data1[i][3:].split(':')[0]
-                    positions = [int(pos) for pos in data1[i][3:].split(':')[1].split(',')]
-                    dict1[doc] = positions
-                    i += 1
-                else:
-                    break
-            while j < len(data2):
-                if data2[j].startswith('   '):
-                    doc = data2[j][3:].split(':')[0]
-                    positions = [int(pos)for pos in data2[j][3:].split(':')[1].split(',')]
-                    dict1[doc] = positions
-                    j += 1
-                else:
-                    break
-            merged.append(f'{word}:{len(dict1)}')
-            for doc in dict1:
-                positions = ' '
-                for pos in dict1[doc]:
-                    positions += f'{pos},'
-                positions = positions[:-1]
-                merged.append(f'   {doc}:{positions}')
-        else:
-            merged.append(data2[j])
-            j += 1
-            while j < len(data2):
+def output(filepath, postings, doc_count):
 
-                if data2[j].startswith('   '):
-                    merged.append(data2[j])
-                    j += 1
-                else:
-                    break
-    if i < len(data1):
-        merged.extend(data1[i:])
-    if j < len(data2):
-        merged.extend(data2[j:])
-    with open(filepath, 'w') as file:
-        for line in merged:
-            file.write(line + '\n')
-    file.close()
+    with open('index.txt', 'w') as outFile:
+        sorted_postings = dict(sorted(postings.items()))
+        for word in sorted_postings.keys():
+            outFile.write(f'{word}:{doc_count[word]}\n')
+            for positions in sorted_postings[word]:
+                outFile.write(' ' * 3 + positions)
 
-def output_from_dict(filepath, postings, doc_count):
-    sorted_postings = dict(sorted(postings.items()))
-    with open(filepath, 'w') as outFile:
-        data1 = outFile.read().splitlines()
-    i=0
-    j=0
-    merged = []
-    while i <len(data1):
-        if data1[i].split(':')[0] < sorted_postings[j]:
-            merged.append(data1[i])
-            i += 1
-            while i < len(data1):
-                if data1[i].startswith('   '):
-                    merged.append(data1[i])
-                    i += 1
-                else:
-                    break
-        elif data1[i].split(':')[0] == sorted_postings[j]:
-            word = data1[i].split(':')[0]
-            i += 1
-            dict1 = {}
-            while i < len(data1) :
-                if data1[i].startswith('   '):
-                    doc = data1[i][3:].split(':')[0]
-                    positions = [int(pos) for pos in data1[i][3:].split(':')[1].split(',')]
-                    dict1[doc] = positions
-                    i += 1
-                else:
-                    break
-            for positions in sorted_postings[j]:
-                doc = positions.split(':')[0]
-                positions = [int(pos) for pos in positions.split(':')[1].split(',')]
-                dict1[doc].append(positions)
-            
-            merged.append(f'{word}:{len(dict1)}')
-            for doc in dict1:
-                dict1[doc] = list(set(sorted(dict1[doc])))
-                positions = ''
-                for pos in dict1[doc]:
-
-                    positions += f'{pos},'
-                positions = positions[:-1]
-                merged.append(f'   {doc}:{positions}')
-            j += 1
-        else:
-            line1 = f'{sorted_postings[j]}:{doc_count[sorted_postings[j]]}\n'
-            merged.append(line1)
-            for positions in sorted_postings[j]:
-                merged.append('   ' + positions)
-            j += 1
-            
-
+    outFile.close()
                 
     
 
@@ -232,7 +62,7 @@ if __name__ == "__main__":
     data = firstThousand(dataset)
     print(data)
     data.to_csv('first1000.csv')
-    add_to_index(index, index_add)
+    output(index, postings, doc_count)
 
     print(get_index('00000'))
     print(get_index('design'))
