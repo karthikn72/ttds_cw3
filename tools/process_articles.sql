f1dcbd409b8174f58c5dedd0a7b01e8b06142120
karthikn72 <karthik-narayan@hotmail.com>
2024-02-18 01:46:42 +0000

    Create preprocessing sql script

---
+++ tools/process_articles.sql
@@ -0,0 +1,20 @@
+
+-- Drop irrelevant date columns
+ALTER TABLE articles DROP COLUMN year, DROP COLUMN month, DROP COLUMN day;
+
+-- Convert date column to timestamp
+ALTER TABLE articles
+   RENAME COLUMN date TO upload_date,
+   ALTER COLUMN upload_date TYPE TIMESTAMP USING TO_TIMESTAMP(upload_date, 'YYYY-MM-DD HH24:MI:SS');
+
+-- Remove special characters from title column
+UPDATE articles
+SET title = REPLACE(REPLACE(title, E'\n', ''), E'\r', '');
+
+-- Remove code and other special characters from article text
+UPDATE articles
+SET article = REPLACE(REPLACE(REGEXP_REPLACE(article, '\{[^}]+\}', '', 'g'), E'\n', ''), E'\r', '');
+
+-- Remove articles with less than 500 characters
+DELETE FROM articles
+WHERE LENGTH(article) < 500;
