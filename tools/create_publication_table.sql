f17fd108c10605ea1484c20a428becca7f7d4c30
Karthik <karthik-narayan@hotmail.com>
2024-02-20 03:13:50 +0000

    Fix publication code

--- tools/create_publication_table.sql
+++ tools/create_publication_table.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS publications;
 
 CREATE TABLE publications (
     publication_id SERIAL PRIMARY KEY,
-    publication_name VARCHAR(255) UNIQUE NOT NULL
+    publication_name VARCHAR(255) UNIQUE
 );
--- tools/update_publications.sql
+++ tools/update_publications.sql
@@ -5,7 +5,7 @@ INSERT INTO publications (publication_name)
 SELECT DISTINCT publication FROM articles;
 
 UPDATE articles
-SET publication_id = p.publication_id
+SET publication = p.publication_id
 FROM publications p
 WHERE articles.publication = p.publication_name;
 
