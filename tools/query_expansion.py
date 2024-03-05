2f5b5a4993cb654d8a5409b4d8c78deac2a457b8
SIMONDSKNIGHT <knigsd@icloud.com>
2024-03-05 11:04:31 +0000

    Update query_expansion.py

--- tools/query_expansion.py
+++ tools/query_expansion.py
@@ -18,6 +18,7 @@ class QueryExpander:
         nltk.download('average_perceptron_tagger')  
 
     def expand_query(self, untokenized_query):  #assumed format of this is that it is a query before it is normalized and stopped, and stemmed and returns a list of tokens with the full token at the start
+        #this version of the query expander uses word pos tagging to get the synonyms of the words in the query
         def _get_wordnet_pos(tag:str) -> str:
 
             if tag[1].startswith('J'):
@@ -32,9 +33,11 @@ class QueryExpander:
         
         #this one assumes a non_tokenized query
         #detect if the query is a list or a string
-        if type(untokenized_query) == SyntaxWarning:
+        if type(untokenized_query) == str:
             tokenizer = Tokenizer(stem=False, stop=True)
             query = tokenizer.tokenize(untokenized_query)
+        else:
+            query = untokenized_query
         
         index =0
         synonyms = []
@@ -65,7 +68,7 @@ class QueryExpander:
         synonyms = clean_synonyms
         output = []
         count = 2 #maximum number of synonyms to add per word
-        output += [untokenized_query]
+        #this can be removed depending on what we want the query expander to give back
         
         output += query
         for synonym in synonyms:
@@ -74,19 +77,21 @@ class QueryExpander:
                 if this_count > 0 and word not in output:
                     output.append(word)
                     this_count -= 1
+        output = ' '.join(output)
+        output = tokenizer.tokenize(output)
+
+
         return output
                 
+#notes to ask karthik 
+    #1.what is the format of the query that is passed to the query expander
+    #2. what is the format of the query that is returned by the query expander
+    #3. how many synonyms do we want to add per word, how would we track this.
 
         
             
-        
 
-#current caveats:
-            #TODO:figure out why the tokeniser returns a tuple #DONE
-            #TODO:LIMIT THE ADDITIONS TO 2 PER WORD  DONE
-            #TODO:FIGURE OUT HOW TO DEAL WITH SYNONYMS WITH SPACES DONE
-            #TODO: FIGURE OUT WHATS NOT WORKING WITH THE TOKENISER SPELLCHECKER 
-            #TODO: FIGURE OUT HOW TO DEAL WITH DOUBLE-TRIPLE BARREL TERMS ???
+
         
 
         
@@ -95,15 +100,17 @@ if __name__ == '__main__':
     
     qe = QueryExpander()
     query = 'call denver direct'
-    print(qe.expand_query2(query))
+    print(qe.expand_query(query))
+    #['call', 'denver', 'direct', 'phone', 'telephone', 'cry', 'outcry', 'yell', 'shout', 'vociferation', 'claim', 'birdcall', 'birdsong', 'song', 'margin', 'option', 'mile', 'high', 'city', 'capital']
     query2 = 'python programming language'
-    print(qe.expand_query2(query2))
-    
+    print(qe.expand_query(query2))
+    #['python', 'programming', 'language', 'scheduling', 'programing', 'computer', 'linguistic', 'communication', 'speech', 'spoken', 'voice', 'oral', 'lyric', 'words', 'process', 'terminology', 'nomenclature']
     query3 = 'data analysis tools'
-    print(qe.expand_query2(query3))
-    
+    print(qe.expand_query(query3))
+    #['data', 'analysis', 'tools', 'information', 'datum', 'point', 'analytic', 'thinking', 'psychoanalysis', 'depth', 'psychology', 'tool', 'instrument', 'creature', 'puppet', 'cock', 'prick', 'dick', 'shaft', 'pecker', 'peter', 'putz']
     query4 = 'machine learning algorithms'
-    print(qe.expand_query2(query4))
-    
+    print(qe.expand_query(query4))
+    #['machine', 'learning', 'algorithms', 'simple', 'political', 'car', 'auto', 'automobile', 'motorcar', 'acquisition', 'eruditeness', 'erudition', 'learnedness', 'scholarship', 'encyclopedism', 'encyclopaedism', 'algorithm', 'algorithmic', 'rule', 'program']
     query5 = 'web development frameworks'
-    print(qe.expand_query2(query5))
+    print(qe.expand_query(query5))
+    #['web', 'development', 'frameworks', 'entanglement', 'vane', 'network', 'world', 'wide', 'www', 'evolution', 'growth', 'growing', 'maturation', 'ontogeny', 'ontogenesis', 'exploitation', 'developing', 'model', 'theoretical', 'account', 'framework', 'fabric']
