8ab67449d69b28f6e818cabce78b6ef03e2be703
SIMONDSKNIGHT <knigsd@icloud.com>
2024-03-10 23:46:48 +0000

    fixing sql, and deleting tools

--- src/tools/databases/create_doc_length_table.sql
+++ src/tools/databases/create_doc_length_table.sql
@@ -1,4 +1,6 @@
-CREATE TABLE index_table (
+DROP TABLE IF EXISTS doc_length_table;
+
+CREATE TABLE doc_length_table (
     article_id INTEGER REFERENCES articles(article_id),
     doc_length INTEGER,
     PRIMARY KEY (article_id)
