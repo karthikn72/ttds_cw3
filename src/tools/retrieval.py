30083626b2051a1af1a6f43c48a9dd3a28e413dc
Niveda Rameshkumar <niveda.rameshk@gmail.com>
2024-03-14 15:10:19 +0000

    Optimize search

--- src/tools/retrieval.py
+++ src/tools/retrieval.py
@@ -65,36 +65,46 @@ class Retrieval:
         t1 = time.time()
         print('To retrieve docs and scores for non-phrase terms', t1-start)
         start = t1
-        
-        query_phrases = list(filter(lambda x: type(x)==list, query_terms))
-        
-        phrase_docs = set([])
-        docs_have_queries = list(map(lambda x: self.__phrase_search(x), query_phrases))
-        
-        t1 = time.time()
-        print('Phrase searches', t1-start)
-        start = t1
-        
-        for x in docs_have_queries:
-            phrase_docs = phrase_docs & x
 
-        new_doc_scores = defaultdict(lambda: 0)
-        for phrase in query_phrases:
-            for w in phrase:
-                for doc in phrase_docs:
-                    if doc not in new_doc_scores:
-                        new_doc_scores[doc] = doc_scores[doc]
-                    new_doc_scores[doc] += self.index[(self.index['word'] == w) & (self.index['article_id'] == doc)]['tfidf'].values[0]
+        query_phrases = list(filter(lambda x: type(x)==list, query_terms))
+
+        if len(query_phrases)>0:
+            phrase_docs = set([])
+            docs_have_phrases = list(map(lambda x: self.__phrase_search(x), query_phrases))
+        
+            for x in docs_have_phrases:
+                if len(phrase_docs)==0:
+                    phrase_docs = x
+                else:
+                    phrase_docs = phrase_docs & x
+        
+            phrase_words = set(self.__flatten_query_terms(query_phrases))
+
+            phrase_docs_scores = self.index[(self.index['word'].isin(phrase_words)) & (self.index['article_id'].isin(phrase_docs))].groupby('article_id').sum()['tfidf'].to_dict()  
+
+            for d in doc_scores:
+                if d in phrase_docs:
+                    phrase_docs_scores[d]+=doc_scores[d]
+
+            doc_scores = phrase_docs_scores
+
+            t1 = time.time()
+            print('Phrase searches', t1-start)
+            start = t1
 
         return doc_scores
 
     def __bow_retrieval(self, query_words, doc_scores, weight=1):
-        for term in query_words:
-            term_index = self.index[self.index['word']==term]
-            for doc in term_index['article_id']:
-                if doc not in doc_scores:
-                    doc_scores[doc] = 0
-                doc_scores[doc] += term_index[term_index.article_id==doc]['tfidf'].values[0] * weight
+        start = time.time()
+        new_doc_scores = self.index[(self.index['word'].isin(query_words))].groupby('article_id').sum()['tfidf'].to_dict()
+        for doc in new_doc_scores:
+            if doc in doc_scores:
+                doc_scores[doc] += new_doc_scores[doc]*weight
+            else:
+                doc_scores[doc] = new_doc_scores[doc]*weight
+        t1 = time.time()
+        print(f'Free words search {query_words}', t1-start)
+        start = t1
 
         return doc_scores
     
@@ -160,17 +170,22 @@ class Retrieval:
 
             if (term1 not in self.index['word'].values)|(term2 not in self.index['word'].values):
                 return set([])
+
+            term1_index = self.index[self.index['word']==term1]
+            term2_index = self.index[self.index['word']==term2]
             
             if i==0:
-                docs1 = set(self.index[self.index['word']==term1]['article_id'].values)
-                term1_dict = {doc: np.asarray(self.index[(self.index['word']==term1)&(self.index['article_id']==doc)]['positions'].values[0]) for doc in docs1} 
+                docs1 = set(term1_index['article_id'].unique())
+                term1_dict = dict(zip(term1_index['article_id'], term1_index['positions']))
+                # term1_dict = {doc: self.index[(self.index['word']==term1)&(self.index['article_id']==doc)]['positions'].values[0] for doc in docs1} 
             else:
                 docs1 = set(output_dict.keys())
                 term1_dict = output_dict
-            docs2 = set(self.index[self.index['word']==term2]['article_id'].values)
-            term2_dict = {doc: np.asarray(self.index[(self.index['word']==term2)&(self.index['article_id']==doc)]['positions'].values[0]) for doc in docs2} 
+            docs2 = set(term2_index['article_id'].unique())
+            term2_dict = dict(zip(term2_index['article_id'], term2_index['positions']))
+            # term2_dict = {doc: np.asarray(self.index[(self.index['word']==term2)&(self.index['article_id']==doc)]['positions'].values[0]) for doc in docs2} 
             shared_docs = docs1 & docs2
-
+            
             output_dict = self.__check_adjacent_words(shared_docs, term1_dict, term2_dict)
 
             if len(output_dict)==0:
@@ -181,8 +196,8 @@ class Retrieval:
     def __check_adjacent_words(self, shared_docs, former_pos_dict, later_pos_dict, proximity=1):
         output_dict = defaultdict(list)
         for doc in shared_docs:
-            term1_positions = former_pos_dict[doc] + proximity
-            term2_positions = later_pos_dict[doc]
+            term1_positions = np.array(former_pos_dict[doc]) + proximity
+            term2_positions = np.array(later_pos_dict[doc])
 
             phrase_pos = np.intersect1d(term1_positions, term2_positions)
 
